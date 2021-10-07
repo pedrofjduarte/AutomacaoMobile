9d08381a88d4b49167d6facbbf21f1e642801b20
Pedro Junio <pedrofjduarte@gmail.com>
2021-10-06 22:47:38 -0300

    Implementação do AppiumDriver na classe DriverFactory. Criação da classe CalculadoraTest e implementação do método de teste validarMultiplicacao.

---
+++ src/test/java/br/com/chronosAcademy/appium/CalculadoraTest.java
@@ -0,0 +1,27 @@
+package br.com.chronosAcademy.appium;
+
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+
+    @Test
+    public void validarMultiplicacao() throws MalformedURLException {
+        DriverFactory driverFactory = new DriverFactory();
+
+        driverFactory.setCapabilities("Android", "ChronosMobile", "uiautomator2"
+                , "com.android.calculator2", "com.android.calculator2.Calculator");
+        driverFactory.setDriver();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_7")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("op_mul")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_9")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+        assertEquals("63", resultado);
+        driverFactory.getDriver().quit();
+    }
+}
