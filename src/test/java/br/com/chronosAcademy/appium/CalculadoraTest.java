37486363f8fd0952f667c38c7216815d9bfd9fc1
Pedro Junio <pedrofjduarte@gmail.com>
2021-10-08 20:13:39 -0300

    Ciação do métodos de testes validarSoma e validarDivisao.

--- src/test/java/br/com/chronosAcademy/appium/CalculadoraTest.java
+++ src/test/java/br/com/chronosAcademy/appium/CalculadoraTest.java
@@ -24,4 +24,36 @@ public class CalculadoraTest {
         assertEquals("63", resultado);
         driverFactory.getDriver().quit();
     }
+
+    @Test
+    public void validarSoma() throws MalformedURLException{
+        DriverFactory driverFactory = new DriverFactory();
+
+        driverFactory.setCapabilities("Android", "ChronosMobile", "uiautomator2"
+                , "com.android.calculator2", "com.android.calculator2.Calculator");
+        driverFactory.setDriver();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_7")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("op_add")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_9")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+        assertEquals("16", resultado);
+        driverFactory.getDriver().quit();
+    }
+
+    @Test
+    public void validarDivisao() throws MalformedURLException{
+        DriverFactory driverFactory = new DriverFactory();
+
+        driverFactory.setCapabilities("Android", "ChronosMobile", "uiautomator2"
+                , "com.android.calculator2", "com.android.calculator2.Calculator");
+        driverFactory.setDriver();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_7")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("op_div")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("digit_9")).click();
+        driverFactory.getDriver().findElement(MobileBy.id("eq")).click();
+        String resultado = driverFactory.getDriver().findElement(MobileBy.id("result")).getText();
+        assertEquals("0.7777777777777", resultado);
+        driverFactory.getDriver().quit();
+    }
 }
